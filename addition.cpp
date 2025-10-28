83792cd4e11cabde9661e575037ceb22d28ceebc
mukulsingh2390 <mukulsingh9146@gmail.com>
2025-10-28 18:39:50 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, sum;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Perform addition
+    sum = num1 + num2;
+
+    // Display the result
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
